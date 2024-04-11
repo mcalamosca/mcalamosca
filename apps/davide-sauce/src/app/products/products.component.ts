4483171fee82e0c0331e8c2bf5e4630a19ddae6a
Micah Calamosca <mcalamosca@gmail.com>
2024-04-11 15:43:01 -0400

    moved products to assets

--- apps/davide-sauce/src/app/products/products.component.ts
+++ apps/davide-sauce/src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '@mcalamosca/ui-components';
+import { products } from '../../assets/products-cards';
 
 @Component({
   selector: 'ds-products',
@@ -10,62 +11,5 @@ import { ProductCardComponent } from '@mcalamosca/ui-components';
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
-  products = [
-    {
-      name: 'Original Tomato',
-      imageUrl: '../../assets/images/originale.jpg',
-      image2Url: '../../assets/images/nutritionfacts.jpg',
-      bgColor: '#29341f',
-      description: "A classic and versatile tomato sauce that's perfect for any dish.",
-      contrastColor: '#dae7c9',
-    },
-    {
-      name: 'Garlic Tomato',
-      imageUrl: '../../assets/images/garlic.jpg',
-      image2Url: '../../assets/images/nutritionfacts.jpg',
-      bgColor: '#29341f',
-      description: 'Rich and aromatic, infused with the robust flavor of garlic.',
-      contrastColor: '#dae7c9',
-    },
-    {
-      name: 'Basil Tomato',
-      imageUrl: '../../assets/images/sweet-basil.jpg',
-      image2Url: '../../assets/images/nutritionfacts.jpg',
-      bgColor: '#29341f',
-      description: 'Fresh and herby, featuring the light, peppery layers of basil.',
-      contrastColor: '#dae7c9',
-    },
-    {
-      name: 'Spicy Tomato',
-      imageUrl: '../../assets/images/saucespicy.jpg',
-      image2Url: '../../assets/images/nutritionfacts.jpg',
-      bgColor: '#29341f',
-      description: 'A fiery blend that adds a kick with its bold, spicy flavors.',
-      contrastColor: '#dae7c9',
-    },
-    {
-      name: 'Wild Mushroom Tomato',
-      imageUrl: '../../assets/images/mushroom.jpg',
-      image2Url: '../../assets/images/nutritionfacts.jpg',
-      bgColor: '#29341f',
-      description: 'Earthy and savory, with the deep, umami-rich taste of wild mushrooms.',
-      contrastColor: '#dae7c9',
-    },
-    {
-      name: 'Sweet Pepper Tomato',
-      imageUrl: '../../assets/images/sweetpepper.jpg',
-      image2Url: '../../assets/images/nutritionfacts.jpg',
-      bgColor: '#29341f',
-      description: 'Sweet and slightly tangy, featuring the vibrant taste of bell peppers.',
-      contrastColor: '#dae7c9',
-    },
-    {
-      name: 'Creamy Tomato',
-      imageUrl: '../../assets/images/creamytomato.jpg',
-      image2Url: '../../assets/images/nutritionfacts.jpg',
-      bgColor: '#29341f',
-      description: 'Smooth and rich, with a luxurious texture and a hint of cream.',
-      contrastColor: '#dae7c9',
-    },
-  ];
+  products = products;
 }
---
+++ apps/davide-sauce/src/assets/products-cards.ts
@@ -0,0 +1,63 @@
+export const products = [
+  {
+    name: "Original Tomato",
+    imageUrl: "../../assets/images/originale.jpg",
+    image2Url: "../../assets/images/nutritionfacts.jpg",
+    bgColor: "#29341f",
+    description:
+      "A classic and versatile tomato sauce that's perfect for any dish.",
+    contrastColor: "#dae7c9",
+  },
+  {
+    name: "Garlic Tomato",
+    imageUrl: "../../assets/images/garlic.jpg",
+    image2Url: "../../assets/images/nutritionfacts.jpg",
+    bgColor: "#29341f",
+    description: "Rich and aromatic, infused with the robust flavor of garlic.",
+    contrastColor: "#dae7c9",
+  },
+  {
+    name: "Basil Tomato",
+    imageUrl: "../../assets/images/sweet-basil.jpg",
+    image2Url: "../../assets/images/nutritionfacts.jpg",
+    bgColor: "#29341f",
+    description:
+      "Fresh and herby, featuring the light, peppery layers of basil.",
+    contrastColor: "#dae7c9",
+  },
+  {
+    name: "Spicy Tomato",
+    imageUrl: "../../assets/images/saucespicy.jpg",
+    image2Url: "../../assets/images/nutritionfacts.jpg",
+    bgColor: "#29341f",
+    description: "A fiery blend that adds a kick with its bold, spicy flavors.",
+    contrastColor: "#dae7c9",
+  },
+  {
+    name: "Wild Mushroom Tomato",
+    imageUrl: "../../assets/images/mushroom.jpg",
+    image2Url: "../../assets/images/nutritionfacts.jpg",
+    bgColor: "#29341f",
+    description:
+      "Earthy and savory, with the deep, umami-rich taste of wild mushrooms.",
+    contrastColor: "#dae7c9",
+  },
+  {
+    name: "Sweet Pepper Tomato",
+    imageUrl: "../../assets/images/sweetpepper.jpg",
+    image2Url: "../../assets/images/nutritionfacts.jpg",
+    bgColor: "#29341f",
+    description:
+      "Sweet and slightly tangy, featuring the vibrant taste of bell peppers.",
+    contrastColor: "#dae7c9",
+  },
+  {
+    name: "Creamy Tomato",
+    imageUrl: "../../assets/images/creamytomato.jpg",
+    image2Url: "../../assets/images/nutritionfacts.jpg",
+    bgColor: "#29341f",
+    description:
+      "Smooth and rich, with a luxurious texture and a hint of cream.",
+    contrastColor: "#dae7c9",
+  },
+];
