886e0fd155366349d82faa304e6527e2ab1a524e
Micah Calamosca <mcalamosca@gmail.com>
2024-04-22 16:41:34 -0400

    updated lightbox for single images

--- libs/ui-components/src/lib/info-section/info-section.component.ts
+++ libs/ui-components/src/lib/info-section/info-section.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ViewContainerRef } from '@angular/core';
+import {Image} from '../grid-gallery/grid-gallery.component';
+import {LightboxComponent} from '../lightbox/lightbox.component';
 
 @Component({
   selector: 'mcui-info-section',
@@ -8,4 +10,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './info-section.component.scss',
 })
 export class InfoSectionComponent {
+  @ViewChild('lightboxContainer', { read: ViewContainerRef, static: true }) lightboxContainer!: ViewContainerRef;
+  @Input() image!: Image;
+
+  openLightbox(image: Image) {
+    const componentRef = this.lightboxContainer.createComponent(LightboxComponent);
+    componentRef.instance.image = image;
+    componentRef.instance.closeEvent.subscribe(() => {
+      componentRef.destroy();
+    });
+  }
+
 }
