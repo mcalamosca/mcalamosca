be766f7cb329482d10d82e1cf5112b5b20f61112
Micah Calamosca <mcalamosca@gmail.com>
2024-04-24 12:23:26 -0400

    improved info section to dynamic template

--- libs/ui-components/src/lib/info-section/info-section.component.ts
+++ libs/ui-components/src/lib/info-section/info-section.component.ts
@@ -1,17 +1,33 @@
-import { Component, Input, ViewChild, ViewContainerRef } from '@angular/core';
-import {Image} from '../grid-gallery/grid-gallery.component';
-import {LightboxComponent} from '../lightbox/lightbox.component';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectorRef,
+  Component,
+  ContentChild,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { Image } from '../grid-gallery/grid-gallery.component';
+import { LightboxComponent } from '../lightbox/lightbox.component';
 
 @Component({
   selector: 'mcui-info-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './info-section.component.html',
   styleUrl: './info-section.component.scss',
 })
 export class InfoSectionComponent {
   @ViewChild('lightboxContainer', { read: ViewContainerRef, static: true }) lightboxContainer!: ViewContainerRef;
   @Input() image!: Image;
+  @Input() imageFirst = true;
+
+
+  @ContentChild('infoText') infoText!: TemplateRef<any>;
+  @ContentChild('infoImage') infoImage!: TemplateRef<any>;
 
   openLightbox(image: Image) {
     const componentRef = this.lightboxContainer.createComponent(LightboxComponent);
@@ -20,5 +36,4 @@ export class InfoSectionComponent {
       componentRef.destroy();
     });
   }
-
 }
