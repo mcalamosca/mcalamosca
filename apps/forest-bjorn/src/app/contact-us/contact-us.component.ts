8a96097139ed2e9e37836d4c63bb93febaa5866d
Micah Calamosca <mcalamosca@gmail.com>
2024-04-30 16:32:03 -0400

    added contact form, landing text

--- apps/forest-bjorn/src/app/contact-us/contact-us.component.ts
+++ apps/forest-bjorn/src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'forest-bjorn-contact-us',
   standalone: true,
-  imports: [],
+  //material form
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatCardModule, MatButtonModule],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.scss'
+  styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent {
+  contactForm = new FormGroup({
+    inquiryType: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+  });
 
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    }
+  }
 }
--- apps/forest-bjorn/src/app/landing/landing.component.ts
+++ apps/forest-bjorn/src/app/landing/landing.component.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {RandomQuoteComponent} from '@mcalamosca/ui-components';
 
 @Component({
   selector: 'forest-bjorn-landing',
+  standalone: true,
+  imports: [CommonModule,MatDividerModule,MatIconModule],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent {
+  showMore = false;
+
 }
